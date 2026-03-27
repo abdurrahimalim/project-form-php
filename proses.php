014cf67ba22abeb6c85bce548c3952ba311df9d7
Azkiya-12 <azkiyafazaputripratama@gmail.com>
2026-03-27 20:06:02 +0700

    tambah proses.php

--- proses.php
+++ proses.php
@@ -1,25 +1,12 @@
 <?php
 require_once 'Person.php';
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$phone = $_POST['phone'];
-$address = $_POST['address'];
-
-$person = new Person($firstname, $lastname, $phone, $address);
+//ambil data jadi array
+$data = $_POST;
+$person = new Person(
+    $data['firstname'],
+    $data['lastname'],
+    $data['phone'],
+    $data['address'],
+);
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hasil</title>
-        <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <p>Hi, my name is <?php echo $person->getFullName(); ?></p>
-        <p>Phone Number: <?php echo $person->getPhone(); ?></p>
-        <p>Address: <?php echo $person->getAddress(); ?></p>
-    </div>
-</body>
-</html>
