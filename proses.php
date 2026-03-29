db2828c8419bc8fd81b5f5f519c122d59191276b
Azkiya-12 <azkiyafazaputripratama@gmail.com>
2026-03-29 21:39:57 +0700

    feat: tambah proses.php

--- proses.php
+++ proses.php
@@ -14,6 +14,23 @@ function prosesForm($data) {
         $address
     );
 }
-$data = $_POST;
-$person = prosesForm($data);
-?>
+<?php
+require_once 'Person.php';
+
+function prosesForm($data) {
+    $firstname = trim($data['firstname']);
+    $lastname = trim($data['lastname']);
+    $phone = trim($data['phone']);
+    $address = trim($data['address']);
+
+    return new Person(
+        $firstname,
+        $lastname,
+        $phone,
+        $address
+    );
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+    $person = prosesForm($_POST);
+}
+
